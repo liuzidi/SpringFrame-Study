bed57bd2e935d1257b391c324cdb668042f02c4e
liuzidi <liuzidi17@163.com>
2021-07-21 17:43:37 +0800

    springMVC

---
+++ lzd-SpringMVC-01/src/main/java/lzd/zju/controller/MyController.java
@@ -0,0 +1,33 @@
+package lzd.zju.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author:liuzidi
+ * @Description 创建处理器对象，对象放在springMVC容器中，再类的上面
+ * 能控制请求的都是控制器对象：如myController能处理请求，又称为后端控制器
+ */
+@Controller
+public class MyController {
+    /**
+     * @RequestMapping 请求映射 作用为把一个地址和一个方法绑定在一起，再方法和类的上面
+     * value = (some.do)必须唯一不重复
+     * 类似于doPost和doGet
+     * 返回值：ModelAndView
+     * Model：数据，请求完成后，返回给用户的数据
+     * View：视图 比如jsp
+     */
+    @RequestMapping(value = "some.do")
+    public ModelAndView doSome(){
+        //处理some.do请求
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("msg","welcome to use springmvc dev");
+        mv.addObject("fun","using doSome method");
+        //指定视图，指定视图的完整路径,路径为WEB-INF下的相对路径
+        //框架对视图进行forward操作，request.getRequestDispather("/show.jsp").forward(...)
+        mv.setViewName("/show.jsp");
+        return mv;
+    }
+}
---
+++ lzd-SpringMVC-01/src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <p>springMVC的项目</p>
+    <p><a href="some.do">发起some.do请求</a> </p>
+</body>
+</html>
